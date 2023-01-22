912f19ad548888f9554e01bc7046f64428f0aaab
KuznetsovKU <kuznetsov_k@outlook.com>
2023-01-22 17:33:33 +0300

    added QuickSort mathod

---
+++ dotnet/SortingTypes/QuickType/Program.cs
@@ -0,0 +1,7 @@
+﻿using static Infrastructure;
+using static Sorting;
+
+int [] testArray = CreateArray(10);
+PrintArray(testArray);
+QuickSort(testArray, 0, testArray.Length-1);
+PrintArray(testArray);
